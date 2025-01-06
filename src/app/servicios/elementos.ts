263d845d5d57a0a0263c11bfb6340038b23f8d61
PabloPayDev <PabloJBecerraVDev@gmail.com>
2025-01-06 11:59:47 -0400

    Pablo - Elementos service fixes

--- src/app/servicios/elementos.ts
+++ src/app/servicios/elementos.ts
@@ -10,54 +10,13 @@ export class ElementosService {
   private URL = config.URL;
 
   constructor(private http: HttpClient) {}
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 // Añadir elementos
 addElemento(elemento: any): Observable<any> {
-console.log( ' elemento reciido en el form corto : ', elemento);
-const params =  {
-    "procedure_name": "sp_proy_elementos",
-    "body": {
-      "params": [        
-        { "name": "p_accion", "value": "A1", "type": "string" },
-        { "name": "p_id_proy_elemento", "value": null, "type": "int" },
-        { "name": "p_id_proyecto", "value": elemento.id_proyecto || null, "type": "int" },
-        { "name": "p_id_meto_elemento", "value": elemento.id_meto_elemento || null, "type": "int" },
-        { "name": "p_id_proy_elem_padre", "value": elemento.id_proy_elem_padre || null, "type": "int" },
-        { "name": "p_codigo", "value": elemento.codigo || null, "type": "string" },
-        { "name": "p_elemento", "value": elemento.elemento || null, "type": "string" },
-        { "name": "p_descripcion", "value": elemento.descripcion || null, "type": "string" },
-        { "name": "p_comentario", "value": elemento.comentario || null, "type": "string" },
-        { "name": "p_nivel", "value": elemento.nivel || null, "type": "int" },
-        { "name": "p_orden", "value": elemento.orden || null, "type": "int" },
-        { "name": "p_idp_estado", "value": elemento.idp_estado || null, "type": "int" },
-        { "name": "p_peso", "value": elemento.peso || null, "type": "int" }
-      ]
-    }
-  };
-
-
-  console.log('params mucho ante de enviar  : ', params);
-  const headers = new HttpHeaders({
-    'ip': "127.0.0.1"
-  });
-
-  return this.http.post<any>(this.URL, params, { headers });
-}
-
-// editar elementos
-editElemento(elemento: any): Observable<any> {
   console.log( ' elemento reciido en el form corto : ', elemento);
   const params =  {
-=======
-=======
->>>>>>> Stashed changes
-  // Añadir elementos
-  addElemento(elemento: any): Observable<any> {
-    const params =  {
       "procedure_name": "sp_proy_elementos",
       "body": {
-        "params": [
+        "params": [        
           { "name": "p_accion", "value": "A1", "type": "string" },
           { "name": "p_id_proy_elemento", "value": null, "type": "int" },
           { "name": "p_id_proyecto", "value": elemento.id_proyecto || null, "type": "int" },
@@ -74,19 +33,19 @@ editElemento(elemento: any): Observable<any> {
         ]
       }
     };
+  
+  
+    console.log('params mucho ante de enviar  : ', params);
     const headers = new HttpHeaders({
       'ip': "127.0.0.1"
     });
+  
     return this.http.post<any>(this.URL, params, { headers });
   }
 
   // editar elementos
   editElemento(elemento: any): Observable<any> {
     const params =  {
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
       "procedure_name": "sp_proy_elementos",
       "body": {
         "params": [
@@ -115,8 +74,6 @@ editElemento(elemento: any): Observable<any> {
   // ======= ======= ======= Obtener Todos los Meto Elementos ======= ======= =======
   getAllElementos(): Observable<any> {
     const params = {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
         "procedure_name": "sp_proy_elementos",
         "body": {
           "params": [      
@@ -136,38 +93,11 @@ editElemento(elemento: any): Observable<any> {
           ]
         }
       };
-
-=======
-=======
->>>>>>> Stashed changes
-      "procedure_name": "sp_proy_elementos",
-      "body": {
-        "params": [
-          { "name": "p_accion", "value": "C1", "type": "string" },
-          { "name": "p_id_proy_elemento", "value": null, "type": "int" },
-          { "name": "p_id_proyecto", "value": null, "type": "int" },
-          { "name": "p_id_meto_elemento", "value": null, "type": "int" },
-          { "name": "p_id_proy_elem_padre", "value": null, "type": "int" },
-          { "name": "p_codigo", "value": null, "type": "string" },
-          { "name": "p_elemento", "value": null, "type": "string" },
-          { "name": "p_descripcion", "value": null, "type": "string" },
-          { "name": "p_comentario", "value": null, "type": "string" },
-          { "name": "p_nivel", "value": null, "type": "int" },
-          { "name": "p_orden", "value": null, "type": "int" },
-          { "name": "p_idp_estado", "value": null, "type": "int" },
-          { "name": "p_peso", "value": null, "type": "int" }
-        ]
-      }
-    };
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     const headers = new HttpHeaders({
       'ip': "127.0.0.1"
     });
     return this.http.post<any>(this.URL, params, { headers });
-  }
+  };
 
   // ======= ======= ======= Obtener  Elementos por Metodología ======= ======= =======
   getElementosByProyecto(idProyecto: number): Observable<any> {
@@ -248,8 +178,4 @@ editElemento(elemento: any): Observable<any> {
     return this.http.post<any>(this.URL, params, { headers });
   }
   // ======= ======= ======= ======= ======= ======= =======
-<<<<<<< Updated upstream
-  // ======= ======= ======= ======= ======= ======= =======
-=======
->>>>>>> Stashed changes
 }
