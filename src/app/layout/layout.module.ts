1ad85c0ac8398e8a54604a2df7852dc98956ded4
Angel Bruno Laura Calliconde <angel.laura@CI-LAPAZ.GOV.BO>
2025-05-02 13:50:52 -0400

    ajuste por correcion en ambiente de producción

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,3 +1,4 @@
+import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
@@ -6,9 +7,12 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
+import { FormsModule } from '@angular/forms';
+import { BodyHeaderComponent } from './components/bodyHeader/bodyHeader.component';
 
 @NgModule({
-    imports: [CommonModule, LayoutRoutingModule, NgbDropdownModule],
-    declarations: [LayoutComponent, SidebarComponent, HeaderComponent]
+    imports: [CommonModule, LayoutRoutingModule, NgbDropdownModule,TranslateModule,FormsModule ],
+    declarations: [LayoutComponent, SidebarComponent, HeaderComponent, BodyHeaderComponent]
+
 })
 export class LayoutModule {}
